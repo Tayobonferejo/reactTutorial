8f8a1b71359fe1b21075d518e3e4db8cc2f6fce6
Tayobonferejo <Oluwatayogeorgeige@gmail.com>
2025-09-20 23:02:24 +0100

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,3 +1,3 @@
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello, React</h1>);
+Hello root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h1>Hello world, React</h1>);
  
